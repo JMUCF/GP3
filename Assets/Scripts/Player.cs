4ca5f2f008dfb6080ae8d77c7728bbff1a6e3db5
JMUCF <ju398578@ucf.edu>
2024-03-11 13:55:08 -0400

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Awake()
+    {
+        //Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
