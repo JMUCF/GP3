aa0bbfb35396fa7be3fbfe1f2d0d99e8607b315d
MBatista18 <149213942+MBatista18@users.noreply.github.com>
2024-03-13 22:43:11 -0400

    Fixed Jumping
    Added jumping

--- Assets/Scripts/JumpFunction.cs
+++ Assets/Scripts/JumpFunction.cs
@@ -5,30 +5,46 @@ using UnityEngine.InputSystem;
 
 public class JumpFunction : MonoBehaviour
 {
-    PlayerControls controls;
+    private PlayerControls playerControls;
     private Rigidbody rb;
     public float jumpHeight = 5f;
+    private bool isGrounded = true; // Track if the player is grounded
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        controls = new PlayerControls();
-        controls.Player.Jump.performed += ctx => Jump();
-    }
-
-    void Jump()
-    {
-        Debug.Log("Jumping");
-        rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        playerControls = new PlayerControls();
+        playerControls.Player.Jump.performed += ctx => OnJump(); // Subscribe to the jump input event
     }
 
     void OnEnable()
     {
-        controls.Player.Enable();
+        if (playerControls != null)
+            playerControls.Enable();
     }
 
     void OnDisable()
     {
-        controls.Player.Disable();
+        if (playerControls != null)
+            playerControls.Disable();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the player is grounded when they collide with something
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnJump()
+    {
+        // Check if the player is grounded before allowing them to jump
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            isGrounded = false; // Player is no longer grounded after jumping
+        }
     }
 }
