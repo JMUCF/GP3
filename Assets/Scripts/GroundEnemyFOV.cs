2b8ac9debd144d45beba53917b55f7b1ddb70464
JMUCF <ju398578@ucf.edu>
2024-04-10 17:24:37 -0400

    Setting up new enemy & attack

---
+++ Assets/Scripts/GroundEnemyFOV.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundEnemyFOV : MonoBehaviour
+{
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+    public List<Transform> visibleTarget = new List<Transform>();
+
+    void Start()
+    {
+        StartCoroutine(FindTarget(.1f));
+    }
+
+    IEnumerator FindTarget(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTarget();
+        }
+    }
+
+    void FindVisibleTarget()
+    {
+        visibleTarget.Clear();
+        Collider[] targetInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+        for (int i = 0; i < targetInViewRadius.Length; i++)
+        {
+            Transform target = targetInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    visibleTarget.Add(target);
+                }
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+            angleInDegrees += transform.eulerAngles.y;
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
