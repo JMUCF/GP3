09950a27c229ee5041dc7d43963b67f42328cd41
JMUCF <ju398578@ucf.edu>
2024-04-29 04:20:48 -0400

    Previous elevators can no longer be entered

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -11,12 +11,14 @@ public class Elevator : MonoBehaviour
     public void Start()
     {
         saveStateScript = GameObject.Find("LevelSaveState").GetComponent<saveState>();
+        Debug.Log("gameObject name: " + gameObject.name);
     }
 
     public void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            Debug.Log("gameObject name: " + gameObject.name);
             if (saveStateScript.inALevel && saveStateScript.telekinesis.leverFlipped)
             {
                 saveStateScript.inALevel = false;
@@ -27,12 +29,12 @@ public class Elevator : MonoBehaviour
                 saveStateScript.inALevel = true;
                 SceneManager.LoadScene("EngineRoom");
             }
-            else if (saveStateScript.levelOnePass && !saveStateScript.levelTwoPass)
+            else if (saveStateScript.levelOnePass && !saveStateScript.levelTwoPass && gameObject.name == "Elevator2Hub")
             {
                 saveStateScript.inALevel = true;
                 SceneManager.LoadScene("Level2");
             }
-            else if (saveStateScript.levelOnePass && saveStateScript.levelTwoPass && !saveStateScript.levelThreePass)
+            else if (saveStateScript.levelOnePass && saveStateScript.levelTwoPass && !saveStateScript.levelThreePass && gameObject.name == "Elevator3Hub")
             {
                 saveStateScript.inALevel = true;
                 SceneManager.LoadScene("Observatory");
